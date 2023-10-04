fe8959bbeaacd5d7f270eabd2ad715237398b795
Endre Márk Borza <endremborza@gmail.com>
2023-10-05 00:08:16 +0200

    Updates

---
+++ _app/immutable/chunks/paths.242c7fac.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_yz8tj0)==null?void 0:s.base)??"/academic-impact-explorer";var a;const t=((a=globalThis.__sveltekit_yz8tj0)==null?void 0:a.assets)??e;export{t as a,e as b};
