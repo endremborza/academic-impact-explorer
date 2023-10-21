a339759484582c8d962859743464da643ab1f77e
Endre Márk Borza <endremborza@gmail.com>
2023-10-21 23:37:06 +0200

    Updates

--- _app/immutable/chunks/constants.30a985e8.js
+++
@@ -1,1 +0,0 @@
-const s="Academic Impact Explorer",a="https://tmp-borza-public-cyx.s3.amazonaws.com/quercus-basis-v2-xlarge",t="Institution",c=10;export{s as A,c as D,t as I,a as S};
---
+++ _app/immutable/chunks/constants.70286ae7.js
@@ -0,0 +1,1 @@
+const t="Academic Impact Explorer",s="http://0.0.0.0:8000",I="Institution",c=10;export{t as A,c as D,I,s as S};
